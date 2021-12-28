be2e60e9aed1588f00b521bc7561ae144b9cfad4
Gustavo Caciolato <gustavo.caciolato@multiar.com.br>
2021-12-28 15:23:04 -0300

    C:/Program Files/Git/users/create

--- index.js
+++ index.js
@@ -19,6 +19,32 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+app.get('/users/create', (req, res) => {
+  res.render('adduser');
+});
+
+app.post('/users/create', async (req, res) => {
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  let newsletter = req.body.newsletter;
+
+  if(newsletter === 'on') {
+    newsletter = true;
+  } else {
+    newsletter = false;
+  }
+
+  console.log(req.body)
+
+  await User.create({
+    name,
+    occupation,
+    newsletter
+  });
+
+  res.redirect('/');
+});
+
 app.get('/', (req, res) => {
   res.render('home');
 });
